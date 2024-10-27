d6a22768ca07aed8111df49a29d9450740213996
Melisa <melamaachia@gmail.com>
2024-10-27 21:29:41 +0000

    first commit

---
+++ controllers/todo-controller.js
@@ -0,0 +1,67 @@
+import { todoModel } from "../models/todo-model.js";
+
+export const addTask = async (req, res) => {
+
+    try {
+        console.log('request', req.body)
+        const addData = await todoModel.create(req.body);
+        res.send(req.body);
+
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+// Get all Tasks
+export const getTasks = async (req, res, next) => {
+    try {
+        const getData = await todoModel.find()
+        {
+            res.status(200).json(getData)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+// Get A Single Task
+export const getTask = async (req, res, next) => {
+    try {
+        const Task = await todoModel.findById (req.params.id)
+        {
+            res.status(200).send(Task)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+// Update a Task
+export const updateTask = async (req, res, next) => {
+    try {
+        const newData = req.body;
+        console.log('request', req.body);
+        const updateData = await todoModel.findByIdAndUpdate(req.params.id, newData, {new:true});
+         res.status(200).send(updateData)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+
+// delete a Task
+
+export const deleteTask = async (req, res, next) => {
+    try {
+        const deleteData = await todoModel.findByIdAndDelete(req.params.id);
+        
+            res.status(200).send('Task has been deleted')
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { todoRouter } from "./routes/todo-router.js";
+import { dbConnection } from "./config/db.js";
+ 
+
+// create express app
+const app = express()
+
+app.use(express.json());
+
+dbConnection();
+
+app.use(todoRouter);
+
+
+
+
+// listening for incoming requests
+const port = 8000
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+});
